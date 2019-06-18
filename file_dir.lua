a678285ea4d69468c28aefd359aa207da43ad369
xxr <1210906435@qq.com>
2019-06-18 16:46:21 +0800

    小改动

--- file_dir.lua
+++ file_dir.lua
@@ -2,10 +2,9 @@ local lfs = require "lfs"
 
 function PrintDir(rootPath)
     for entry in lfs.dir(rootPath) do
-        if entry~='.' and entry~='..' then
+        if entry~='.' and entry~='..' then 
             local path = rootPath.."\\"..entry
             local attr = lfs.attributes(path)
-            assert(type(attr)=="table") --如果获取不到属性表则报错
             if(attr.mode == "directory") then
                 print("Dir:",path)
             elseif attr.mode=="file" then
